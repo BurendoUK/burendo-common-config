a55b8e248b88efcdb4856f797ce4cfce37896ff1
Chris Scott-Thomas <chris.scott-thomas@burendo.com>
2022-08-09 10:35:20 +0100

    Drop unused ACL
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@burendo.com>

--- s3.tf
+++ s3.tf
@@ -32,9 +32,3 @@ resource "aws_s3_bucket_public_access_block" "block" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
-
-
-# resource "aws_s3_bucket_acl" "terraform_state_acl" {
-#   bucket = aws_s3_bucket.terraform_state.id
-#   acl    = "private"
-# }
